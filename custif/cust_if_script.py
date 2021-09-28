b7bbf19ab94227dfaa7b1d284e9900c8ab961822
BennyB <student@bchd.3aa0721c-d8a3-400f-b8d2-9652a5616c0d>
2021-09-28 17:10:33 +0000

    While looping

---
+++ custif/cust_if_script.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+message = 'The movie is about to begin, we recommend '
+
+# wrap value in a float() to accept decimals as numbers
+connection = value(input("What is the movie you wish to view?"))
+
+# if input value was equal to 1
+if input = 1:
+    message = message + 'Harry Potter.'
+elif value = 2:
+    message = message + 'Hobbit & Lord of the Rings.'
+elif value = 3:
+    message = message + 'Chronicles of Narnia.'
+elif value = 4:
+    message = message + 'Indiana Jones.'
+else:
+    message = message + 'Movie Not Found.'
+print(message)
+

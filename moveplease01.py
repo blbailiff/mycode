c07603a8226763b1d1cd57c63b018713a6b52fce
BennyB <student@bchd.3aa0721c-d8a3-400f-b8d2-9652a5616c0d>
2021-09-28 14:52:07 +0000

    renaming and moving files

--- moveplease01.py
+++ moveplease01.py
@@ -1,22 +1,14 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
+import shutil
 
-"""A simple script to move two files into ceph_storage/ Alta3 Research | rzfeeser@alta3.com"""
+import os
 
+os.chdir('/home/student/mycode/')
 
- # standard library imports
-  import shutil   # shell utilities will be used to move files
-  import os       # provies access to low level os operations (agnostic to flavor of OS)
+shutil.move('raynor.obj', 'ceph_storage/')
 
- def main():
-     """called at runtime"""
-     os.chdir('/home/student/mycode/')   # move into this working directory
-     shutil.move('raynor.obj', 'ceph_storage/')  # try moving the file raynor.obj into ceph_storage/ dir
+xname = input('What is the new name for kerrigan.obj? ')
 
-     # program will pause while we accept input
-     xname = input('What is the new name for kerrigan.obj? ') # collect string input from the user
-     shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)  # moving kerrigan.obj into
-                                                                        # ceph_storage/ with new name
+shutil.move('ceph_storage/kerrigan.obj', 'ceph_storage/' + xname)
 
-
-main() # this calls our main function
 

b065c8b209616144747079ae1747f8529585495b
BennyB <student@bchd.3aa0721c-d8a3-400f-b8d2-9652a5616c0d>
2021-10-01 15:32:47 +0000

    Running Python Scripts with Crontab

---
+++ networkscript.py
@@ -0,0 +1,4 @@
+# EXAMPLE - networkscript.py will run every 5 minutes 
+0-59/5 * * * * /usr/bin/python3 /home/student/mycode/networkscript.py
+30 * * * * /usr/bin/python3 /home/student/mycode/cron-work/cron1.py >> /tmp/log.pylog
+
